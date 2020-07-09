c37f4a83f6fa845fb597fe458ea67639ed01f825
Raji Rasheed <ratioraji@gmail.com>
2020-07-09 15:51:14 +0100

    feat/add ng zorro suite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -16,6 +16,18 @@ import { ShellModule } from './shell/shell.module';
 import { LoginModule } from './login/login.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+/**
+ * Locale Registration
+ */
+import { registerLocaleData } from '@angular/common';
+import { default as localeEn } from '@angular/common/locales/en';
+import { NZ_I18N, en_US as localeZorro } from 'ng-zorro-antd';
+const LOCALE_PROVIDERS = [
+  { provide: LOCALE_ID, useValue: 'en' },
+  { provide: NZ_I18N, useValue: localeZorro }
+];
+registerLocaleData(localeEn, 'en');
 
 @NgModule({
   imports: [
@@ -34,7 +46,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule // must be imported as the last module as it contains the fallback route
   ],
   declarations: [AppComponent],
-  providers: [],
+  providers: [...LOCALE_PROVIDERS],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoaderComponent } from './loader/loader.component';
+import { AntdModule } from 'src/app/antd.module';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+
+const MODULES = [CommonModule, RouterModule, AntdModule, TranslateModule];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [...MODULES],
   declarations: [LoaderComponent],
-  exports: [LoaderComponent]
+  exports: [...MODULES, LoaderComponent]
 })
 export class SharedModule {}
